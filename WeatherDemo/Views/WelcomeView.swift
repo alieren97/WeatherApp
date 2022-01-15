d6f9ccd18a3d796e5de7498878e9d8ca76c916ef
alieren97 <alierensh3@gmail.com>
2022-01-16 00:37:08 +0300

    first commit

---
+++ WeatherDemo/Views/WelcomeView.swift
@@ -0,0 +1,37 @@
+//
+//  WelcomeView.swift
+//  WeatherDemo
+//
+//  Created by Ali Eren on 15.01.2022.
+//
+
+import SwiftUI
+import CoreLocationUI
+
+struct WelcomeView: View {
+    @EnvironmentObject var locationManager : LocationManager
+    var body: some View {
+        VStack(){
+            VStack(spacing:20){
+            
+            Text("Welcome to the Weather App").bold().font(.title)
+            Text("Please share your current location to get weather in your area").padding()
+        }.multilineTextAlignment(.center)
+            .padding()
+            
+            LocationButton(.shareCurrentLocation){
+                locationManager.requestLocation()
+            }
+            .cornerRadius(30)
+            .symbolVariant(.fill)
+            .foregroundColor(.white)
+          
+        }.frame(maxWidth:.infinity,maxHeight: .infinity)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Ali Eren on 15.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
